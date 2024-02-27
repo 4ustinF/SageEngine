53b87e355414e2d80bda0d28bf1c4c64872aedc0
William Clark <Austin.farback9@gmail.com>
2024-02-26 22:23:30 -0800

    [Main Branch] - Added ML Neural Network

--- S.A.G.E/Framework/ML/Inc/ML.h
+++ S.A.G.E/Framework/ML/Inc/ML.h
@@ -2,12 +2,15 @@
 
 #include "Common.h"
 
+// ANN Headers
+#include "NeuralNetwork.h"
+
+// Cluster Model Headers
+#include "KMeans.h"
+
 // Datasets Headers
 #include "Datasets.h"
 
 // Linear Model Headers
 #include "LinearRegression.h"
 #include "LogisticRegression.h"
-
-// Cluster Model Headers
-#include "KMeans.h"
---
+++ S.A.G.E/Framework/ML/Inc/NeuralNetwork.h
@@ -0,0 +1,51 @@
+#pragma once
+
+namespace SAGE::ML
+{
+	class Neuron;
+	using Layer = std::vector<Neuron>;
+
+	struct Connection
+	{
+		float weight;
+	};
+
+	class Neuron
+	{
+	public:
+		Neuron(size_t numOutputs, size_t myIndex);
+
+		void SetOutputValue(float value) { mOutputValue = value; }
+		float GetOutputValue() const { return mOutputValue; }
+
+		void FeedForward(const Layer& previousLayer);
+		void CalculateOutputGradients(float targetValue);
+		void CalculateHiddenGradients(const Layer& nextLayer);
+		void UpdateInputWeights(Layer& previousLayer);
+
+	private:
+		std::vector<Connection> mOutputeWeights;
+		size_t mMyIndex = 0;
+		float mOutputValue = 0.0f;
+		float mGradient = 0.0f;
+	};
+
+	class NeuralNetwork
+	{
+	public:
+		NeuralNetwork(const std::vector<size_t>& topology);
+
+		void FeedForward(const std::vector<float>& inputValues);
+		void BackPropagate(const std::vector<float>& targetValues);
+		std::vector<float> GetResults() const;
+
+	private:
+		std::vector<Layer> mLayers;
+	};
+
+}
+
+// Topology - Structure of the neural network
+// array<int>
+// 2, 2, 3
+// Number for each layer

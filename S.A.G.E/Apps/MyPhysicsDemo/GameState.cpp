1e55fc344e9f7ddc38ff83dbaf9a1b824081bacf
William Clark <Austin.farback9@gmail.com>
2026-05-21 14:44:00 -0700

    [Main] - Created MyPhsyicsDemo Project

---
+++ S.A.G.E/Apps/MyPhysicsDemo/GameState.cpp
@@ -0,0 +1,55 @@
+#include "GameState.h"
+
+using namespace SAGE;
+using namespace SAGE::Physics;
+using namespace SAGE::Graphics;
+using namespace SAGE::Math;
+using namespace SAGE::Input;
+
+namespace
+{
+	// Check for custom components
+	bool OnMake(const char* componentName, const rapidjson::Value& value, GameObject& gameObject)
+	{
+		return false;
+	}
+}
+
+void GameState::Initialize()
+{
+	mGameWorld.AddService<CameraService>();
+	mGameWorld.AddService<RenderService>()->SetSampleFilter(Sampler::Filter::Point);
+
+	mGameWorld.Initialize(100);
+
+	GameObjectFactory::SetMakeOverride(OnMake);
+	//mGameWorld.LoadLevel("../../Assets/Level/your_level.json");
+
+	mGameWorld.GetService<RenderService>()->GetDirectionalLight().ambient = { 0.4f, 0.4f, 0.4f, 1.0f };
+}
+
+void GameState::Terminate()
+{
+	mGameWorld.Terminate();
+}
+
+void GameState::Update(float deltaTime)
+{
+	mGameWorld.Update(deltaTime);
+}
+
+void GameState::Render()
+{
+	mGameWorld.Render();
+
+	SimpleDraw::Render(mGameWorld.GetService<CameraService>()->GetCamera());
+}
+
+void GameState::DebugUI()
+{
+	mGameWorld.DebugUI();
+
+	ImGui::Begin("Debug Control", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+
+	ImGui::End();
+}
---
+++ S.A.G.E/Apps/MyPhysicsDemo/GameState.h
@@ -0,0 +1,15 @@
+#include <SAGE/Inc/SAGE.h>
+
+class GameState : public SAGE::AppState
+{
+public:
+	void Initialize() override;
+	void Terminate() override;
+
+	void Update(float deltaTime) override;
+	void Render() override;
+	void DebugUI() override;
+
+private:
+	SAGE::GameWorld mGameWorld;
+};

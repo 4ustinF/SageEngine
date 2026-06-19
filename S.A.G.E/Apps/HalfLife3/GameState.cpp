94d643010f80179dc222b8c3a6037aadf64b87c2
William Clark <Austin.farback9@gmail.com>
2026-06-19 09:45:27 -0700

    [Main] - Created HL3 project
    - Who knows what will come from this?

---
+++ S.A.G.E/Apps/HalfLife3/GameState.cpp
@@ -0,0 +1,54 @@
+#include "GameState.h"
+
+using namespace SAGE;
+using namespace SAGE::Physics;
+using namespace SAGE::Graphics;
+using namespace SAGE::Math;
+using namespace SAGE::Input;
+
+namespace
+{
+	// Check for custom components
+	bool OnMake(const char* componentName, const rapidjson::Value& value, GameObject& gameObject)
+	{
+		return false;
+	}
+}
+
+void GameState::Initialize()
+{
+	mGameWorld.AddService<CameraService>();
+	mGameWorld.AddService<RenderService>()->SetSampleFilter(Sampler::Filter::Point);
+	//mGameWorld.AddService<RBPhysicsService>();
+	mGameWorld.Initialize(1000);
+
+	GameObjectFactory::SetMakeOverride(OnMake);
+	mGameWorld.LoadLevel("../../Assets/Level/HalfLife/halflife_level.json");
+	mGameWorld.CreateGameObject("../../Assets/Templates/empty.json");
+}
+
+void GameState::Terminate()
+{
+	mGameWorld.Terminate();
+}
+
+void GameState::Update(float deltaTime)
+{
+	mGameWorld.Update(deltaTime);
+}
+
+void GameState::Render()
+{
+	mGameWorld.Render();
+
+	SimpleDraw::Render(mGameWorld.GetService<CameraService>()->GetCamera());
+}
+
+void GameState::DebugUI()
+{
+	mGameWorld.DebugUI();
+
+	ImGui::Begin("Debug Control", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+
+	ImGui::End();
+}

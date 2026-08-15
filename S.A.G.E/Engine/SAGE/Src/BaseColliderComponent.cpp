918110e78766c5d5f8e6aed35af13c66bec2394a
William Clark <Austin.farback9@gmail.com>
2026-08-15 14:09:35 -0700

    Update BaseColliderComponent.cpp

--- S.A.G.E/Engine/SAGE/Src/BaseColliderComponent.cpp
+++ S.A.G.E/Engine/SAGE/Src/BaseColliderComponent.cpp
@@ -54,6 +54,7 @@ void BaseColliderComponent ::Terminate()
 	mPhysicsObject = nullptr;
 	mTransformComponent = nullptr;
 	mRigidBodyComponent = nullptr;
+	mMeshFilterComponent = nullptr;
 	mPhysicsService = nullptr;
 }
 
